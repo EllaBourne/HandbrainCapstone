8257bad945924fc8c6c46132868c6aafc206d7d9
clara_dawang <dawangc@mcmaster.ca>
2026-01-22 11:44:21 -0500

    espnow communication between two mcu on laptop

--- Receiver/Receiver_MCU/src/main.cpp
+++ Receiver/Receiver_MCU/src/main.cpp
@@ -1,4 +1,4 @@
-#include <WiFi.h>
+/*#include <WiFi.h>
 
 void setup(){
   Serial.begin(115200);
@@ -11,4 +11,55 @@ void setup(){
 
 void loop(){
 
+}*/
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#include <esp_now.h>
+#include <WiFi.h>
+
+// structure matches transmitter
+typedef struct imuReadings {
+  float acc_x, acc_y, acc_z;
+  float gyr_x, gyr_y, gyr_z;
+} imuReadings;
+
+imuReadings imuData;
+
+// callback function that runs when data is received
+//uint8_t * mac= pointer to mac address, uint8_t * incomingData = pointer to incoming raw bytes
+void receivedStatus(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&imuData, incomingData, sizeof(imuData)); //copy raw bytes into struct format
+  
+  // printing 6 values
+  Serial.printf("%.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n",
+                imuData.acc_x, imuData.acc_y, imuData.acc_z,
+                imuData.gyr_x, imuData.gyr_y, imuData.gyr_z);
+}
+ 
+void setup() {
+  // init serial monitor
+  Serial.begin(115200);
+  
+  // setting device as wifi station
+  WiFi.mode(WIFI_STA);
+
+  // initializing espnow protoocols
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+//tells espnow to call function when data arrives, usies register receive vallback function
+//this is what makes it automatic
+  esp_now_register_recv_cb(esp_now_recv_cb_t(receivedStatus));
+  
+  Serial.println("Receiver ready - waiting for IMU data...");
+}
+ 
+void loop() {
+  //dont need bc of callback
 }
--- Transmitter/Transmitter_MCU/src/main.cpp
+++ Transmitter/Transmitter_MCU/src/main.cpp
@@ -1,4 +1,4 @@
-#include <Arduino.h>
+/*#include <Arduino.h>
 #include <Wire.h>
 #include "I2C_MPU6886.h"
 
@@ -27,4 +27,95 @@ void loop() {
     Serial.printf("%.2f, %.2f, %.2f, %.2f, %.2f, %.2f\n", acc_x, acc_y, acc_z, gyr_x, gyr_y, gyr_z);
 
     delay(100); // 10 samples/sec
+}*/
+/*
+  Rui Santos & Sara Santos - Random Nerd Tutorials
+  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+#include <esp_now.h>
+#include <WiFi.h> //espnow needs the wifi functions but doesnt rely on wifi
+#include <Arduino.h>
+#include <Wire.h>
+#include "I2C_MPU6886.h"
+
+//pins defiend for the pico
+#define SDA_PIN 26 //I2C serial)
+#define SCL_PIN 32 //I2C clock
+
+I2C_MPU6886 imu(I2C_MPU6886_DEFAULT_ADDRESS, Wire); //imu object
+
+
+// receiver mac address
+uint8_t broadcastAddress[] = {0xD4, 0xD4, 0xDA, 0x98, 0x0D, 0xFC};
+
+typedef struct imuReadings {
+    float acc_x,acc_y, acc_z;
+    float gyr_x, gyr_y, gyr_z;
+} imuReadings;
+
+imuReadings imuData; //new variable of type imuReadings
+
+esp_now_peer_info_t peerInfo; //espnow connection info object (?)
+
+// callback when data is sent,autoruns after each transmission to say if successful or not
+void sentStatus(const uint8_t *mac_addr, esp_now_send_status_t status) {
+    Serial.print("\r\nLast Packet Send Status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+ 
+void setup() {
+    
+    Serial.begin(115200);
+    Wire.begin(SDA_PIN, SCL_PIN);
+    imu.begin();
+ 
+    WiFi.mode(WIFI_STA); //set device as wifi station 
+
+    //initialize espnow
+    if (esp_now_init() != ESP_OK) { //start protocol, if not successfull, print error
+        Serial.println("Error initializing ESP-NOW");
+        return;
+    }
+
+    // after successfull init, register for Send CB  (callback get called auto when something happens) to get status of trasnmitted packet
+    //after ESP send data, immediately call sentStatus
+    esp_now_register_send_cb(esp_now_send_cb_t(sentStatus));
+  
+    //setting up connection to receiver
+    //peerInfo.peer_addr = peer info MAC address field
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6); //6 = 6 bytes
+    peerInfo.channel = 0;  //wifi channel 0 allows auto selection
+    peerInfo.encrypt = false; //dont need encryption 
+  
+    //creating peer connection, if fails print error, if succeeds, done setup
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+        Serial.println("Failed to add peer");
+        return;
+    }
+}
+ 
+void loop() {
+    //reading imu data from sensor 
+    imu.getAccel(&imuData.acc_x, &imuData.acc_y, &imuData.acc_z);
+    imu.getGyro(&imuData.gyr_x, &imuData.gyr_y, &imuData.gyr_z);
+  
+    /*//for debugging
+    Serial.printf("Acc: %.2f, %.2f, %.2f , Gyro: %.2f, %.2f, %.2f\n",
+            imuData.acc_x, imuData.acc_y, imuData.acc_z,
+            imuData.gyr_x, imuData.gyr_y, imuData.gyr_z);
+    */
+  
+  // using espnow send function
+  //esp_err_t = data type for ESP error
+  //uint8_t * esnures sending as raw bytes
+  esp_err_t status = esp_now_send(broadcastAddress, (uint8_t *) &imuData, sizeof(imuData));
+  if (status == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(70);
 }
